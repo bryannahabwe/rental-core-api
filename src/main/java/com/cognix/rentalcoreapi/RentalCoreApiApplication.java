8d05da8a454fc54b912a160a2b006a2d733cac56
Nahabwe Brian <nahabwebryn@hotmail.com>
2026-04-01 10:35:08 +0300

    Setup the initial dependencies of the project

---
+++ src/main/java/com/cognix/rentalcoreapi/RentalCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.cognix.rentalcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cognix/rentalcoreapi/RentalCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognix.rentalcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
